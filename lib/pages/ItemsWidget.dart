f6603288d78e9d6b95e9938ce6b8b3aa024df6ea
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-10-08 20:10:57 +0700

    flutter

--- lib/pages/ItemsWidget.dart
+++ lib/pages/ItemsWidget.dart
@@ -2,44 +2,51 @@ import 'package:flutter/material.dart';
 
 class ItemsWidget extends StatelessWidget {
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return GridView.count(
       childAspectRatio: 0.68,
-      // physics: NeverScrollableScrollPhysics(),
-      crossAxisCount: 2, shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      shrinkWrap: true,
       children: [
-        Container(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 10),
-          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            children: [
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF4C53A5),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Text(
-                      '-50%',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+        for (int i = 1; i < 6; i++)
+          Container(
+            padding: EdgeInsets.only(left: 15, right: 15, top: 10),
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF4C53A5),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        '-50%',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  Icon(
-                    Icons.favorite_border,
-                    color: Colors.red,
-                  ),
-                  InkWell(
+                    Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10), // Thêm khoảng cách giữa Row và hình ảnh
+                Center(
+                  child: InkWell(
                     onTap: () {},
                     child: Container(
                       margin: EdgeInsets.all(10),
@@ -50,16 +57,47 @@ class ItemsWidget extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Container(
-                    padding: EdgeInsets.only(bottom: 8),
-                    alignment: Alignment.centerLeft,
-                    child: Text("Product Title"),
+                ),
+                SizedBox(
+                    height: 10), // Thêm khoảng cách giữa hình ảnh và văn bản
+                Text(
+                  "Product Title",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFF4C53A5),
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(height: 5), // Khoảng cách giữa tiêu đề và mô tả
+                Text(
+                  "Write description of product",
+                  style: TextStyle(
+                    color: Color(0xFF4C53A5),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "\$55",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xFF4C53A5),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.shopping_cart_checkout,
+                        color: Color(0xFF4C53A5),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
       ],
     );
   }
